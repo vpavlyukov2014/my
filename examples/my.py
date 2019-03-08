95e50dc25c54ba027ad5739605629eaf987e6f0b
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-08 21:31:24 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -7,7 +7,7 @@ from luma.core.render import canvas
 def main():
     x_start = 0
     y_start = 0
-    char_ht = 10
+    char_ht = 100
     device = get_device()
     for link in range(0, 4):
       for _ in range(10):
@@ -19,7 +19,7 @@ def main():
                 ht = 2*i + 1
                 x_off = 3*i + 1
                 if link >= (i+1):
-                    draw.rectangle(((x_start + x_off), (y_start + char_ht - (1+ht)), 1, ht), fill="white")
+                    draw.rectangle(((x_start + x_off), (y_start + char_ht - (1+ht)), 2, ht), fill="white")
 
 
 if __name__ == "__main__":
