fed1c9940109057dc22239453aa3991d1ef26e49
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-08 21:01:19 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -17,14 +17,14 @@ def main():
             if link >= (i+1):
                 draw.rectangle(((x_start + x_off), (y_start + char_ht - (1+ht)), 2, ht), fill="white")
         time.sleep(5)
-        draw.text((device.width - 10, 30 + 16), 'World!', fill="purple")
-        time.sleep(20)
-        for x in range(40):
-            with canvas(device) as draw:
-                now = datetime.datetime.now()
-                draw.text((x, 4), str(now.date()), fill="white")
-                draw.text((10, 16), str(now.time()), fill="white")
-                time.sleep(0.1)
+        # draw.text((device.width - 10, 30 + 16), 'World!', fill="purple")
+        # time.sleep(20)
+        # for x in range(40):
+        #     with canvas(device) as draw:
+        #         now = datetime.datetime.now()
+        #         draw.text((x, 4), str(now.date()), fill="white")
+        #         draw.text((10, 16), str(now.time()), fill="white")
+        #         time.sleep(0.1)
 
 if __name__ == "__main__":
     try:
