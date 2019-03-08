d8c78b625f80f904045cc70109437bd295c50abf
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-08 22:05:51 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -7,17 +7,21 @@ from luma.core.render import canvas
 def main():
     x_start = 0
     y_start = 0
-    char_ht = 10
+    h = 10
+    w = 3
+    s = 1
     device = get_device()
     for link in range(0, 4):
       for _ in range(3):
         with canvas(device) as draw:
             print("Testing ramka...")
             time.sleep(1)
-            draw.rectangle((0, 0, link, 10), fill="red")
-            # for i in range(0, link):
-            #     x_off = i*3
-            #     draw.rectangle(((x_start + x_off), (y_start + char_ht - (1+ht)), 1, ht), fill="white")
+            for i in range(0, link):
+                x0 = x_start + (w + s)*i
+                y0 = y_start
+                x1 = x_start + w + (s + w)*i
+                y1 = h * i + 1
+                draw.rectangle((x0, y0, x1, y1), fill="white")
 
 
 if __name__ == "__main__":
