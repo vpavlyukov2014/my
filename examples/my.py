ad259d4509f000a0e4711d2361179049ffbb2567
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-10 21:00:48 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -12,6 +12,7 @@ locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 from demo_opts import get_device
 from luma.core.render import canvas
 
+
 def main():
     wifi_level = 2
     device = get_device()
@@ -22,17 +23,21 @@ def main():
             clock(device, draw)
             time.sleep(1)
 
+
 def clock(device, draw):
     left_padding = 25
     now = datetime.datetime.now()
-    today_time = now.strftime("%d %B %Y, %A  %H:%M:%S ")
-    draw.text((left_padding, 0), encode_text(today_time), font=font(10), fill="yellow")
+    today_time = encode_text(now.strftime("%d %B %Y, %A  %H:%M:%S "))
+    draw.text((left_padding, 0), today_time, font=font(10), fill="yellow")
+
 
 def font(size):
-    make_font('Verdana.ttf', size)
+    return make_font('Verdana.ttf', size)
+
 
 def encode_text(text):
-     unicode(text, 'utf-8')
+     return unicode(text, 'utf-8')
+
 
 def wifi_siganl(device, draw, wifi_level):
     y_start = 0
@@ -54,6 +59,7 @@ def wifi_siganl(device, draw, wifi_level):
         draw.rectangle((x0, y0, x1, y1), fill=color)
         text_val = "%s" % wifi_level
         draw.text((0,0), text_val, fill=color)
+
 
 def make_font(name, size):
     font_path = os.path.abspath(os.path.join(
