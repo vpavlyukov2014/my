6c9630c9cf68aeeaf324e4ee9cf74c658afb0bc6
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-11 19:32:58 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -11,9 +11,10 @@ locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 
 from demo_opts import get_device
 from luma.core.render import canvas
-from luma.core.legacy import show_message
 from luma.core.legacy import text
 from luma.core.legacy.font import proportional, LCD_FONT
+from luma.core.virtual import viewport
+from luma.core.sprite_system import framerate_regulator
 
 
 def main():
@@ -31,9 +32,7 @@ def main():
 def clock(device, draw):
     left_padding = 10
     now = datetime.datetime.now()
-    today_time = now.strftime("%d %B %Y, %A  %H:%M:%S ")
-    # draw.text((left_padding, 0), today_time, font=font(12), fill="white")
-    # text(draw, xy, txt, fill=None, font=None)
+    today_time = now.strftime("%d %B %Y  %A  %H:%M:%S ")
     text(draw, (left_padding, 0 ), today_time, fill="white", font=proportional(LCD_FONT) )
 
 
@@ -41,20 +40,7 @@ def show_text_message(device):
     msg = 'sdfsdfsfdsdfssdfsdfsdf sdfsdfs dfs df sdf sd fs df sdf sd fs dfsdfsdfsdf'
     # device, msg, y_offset=0, fill=None, font=None,scroll_delay=0.03
     # show_message(device, msg, y_offset=30, fill="white", font=prop_font, scroll_delay=0.03)
-    show_message(device, msg, y_offset=20, fill="white", font=proportional(LCD_FONT))
-
-
-# def prop_font():
-    # return proportional(LCD_FONT)
-
-
-def font(size):
-    return make_font('Impact.ttf', size)
-
-
-# def encode_text(text):
-    # return text.decode('cp1251')
-     # return unicode(text, 'utf-8')
+    show_message(device, msg, y_offset=20, fill="white", font=proportional(LCD_FONT), scroll_delay=0.03)
 
 
 def wifi_siganl(device, draw, wifi_level):
@@ -75,14 +61,40 @@ def wifi_siganl(device, draw, wifi_level):
         x1 = x_start + w + (s + w)*i
         y1 = h * i + 1
         draw.rectangle((x0, y0, x1, y1), fill=color)
-        text_val = "%s" % wifi_level
-        draw.text((0,0), text_val, fill=color)
 
 
-def make_font(name, size):
-    font_path = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), 'fonts', name))
-    return ImageFont.truetype(font_path, size)
+def show_message(device, msg, y_offset=0, fill=None, font=None,
+                 scroll_delay=0.03):
+    """
+    Scrolls a message right-to-left across the devices display.
+
+    :param device: The device to scroll across.
+    :param msg: The text message to display (must be ASCII only).
+    :type msg: str
+    :param y_offset: The row to use to display the text.
+    :type y_offset: int
+    :param fill: The fill color to use (standard Pillow color name or RGB
+        tuple).
+    :param font: The font (from :py:mod:`luma.core.legacy.font`) to use.
+    :param scroll_delay: The number of seconds to delay between scrolling.
+    :type scroll_delay: float
+    """
+    fps = 0 if scroll_delay == 0 else 1.0 / scroll_delay
+    regulator = framerate_regulator(fps)
+    with canvas(device) as draw:
+        w, h = textsize(msg, font)
+
+    x = device.width
+    virtual = viewport(device, width=w + x + x, height=10) #device.height)
+
+    with canvas(virtual) as draw:
+        text(draw, (x, y_offset), msg, font=font, fill=fill)
+
+    i = 0
+    while i <= w + x:
+        with regulator:
+            virtual.set_position((i, 0))
+            i += 1
 
 
 if __name__ == "__main__":
