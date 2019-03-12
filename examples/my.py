b1981717300e1e13ce59c93f6da617d6ae734f0d
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-12 19:21:15 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -57,7 +57,7 @@ def progress_bar(device, draw, completed):
     d2_y2 = d1_y2 + h2
     draw.rectangle((d2_x1, d2_y1, d2_x2, d2_y2), fill=color2)
 
-    d3_x1 = d2_x2
+    d3_x1 = d2_x2 + 1
     d3_y1 = d1_y1
     d3_x2 = display_w
     d3_y2 = d1_y2
