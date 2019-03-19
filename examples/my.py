3a0a87370b7a7b25dd88a8e28ed3fc9f51c42eb1
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-19 19:27:55 +0300

    hello2

--- examples/my.py
+++ examples/my.py
@@ -26,7 +26,7 @@ def main():
             progress_bar(device, draw)
             music_timer(device, draw)
             draw_status_sym(device, draw, i)
-            draw_title(device, draw)
+            draw_title(device, draw, i)
             time.sleep(1)
 
 def volumeo_info():
