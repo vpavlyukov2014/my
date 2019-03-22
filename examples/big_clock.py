cbe20746efb8c55162109cc35971c872deef88bf
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-03-22 17:37:08 +0300

    hello2

--- examples/big_clock.py
+++ examples/big_clock.py
@@ -16,8 +16,8 @@ from PIL import ImageFont
 def main():
     device = get_device()
     clock = ClockText()
-    font1 = make_font("Andale_Mono.ttf", 14)
-    font2 = make_font("Andale_Mono.ttf", 60)
+    font1 = make_font("arialbi.ttf", 14)
+    font2 = make_font("arialbi.ttf", 60)
 
     try:
         while True:
