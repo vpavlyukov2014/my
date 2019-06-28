d000d47ba9902e0119539615eb2bce6009378de5
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-06-28 23:41:32 +0300

    dfewfw

--- examples/power_relay.py
+++ examples/power_relay.py
@@ -8,7 +8,7 @@ class PowerRelay():
     def __init__(self):
         self.channel = 21
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.channel, GPIO.OUT)
+        # GPIO.setup(self.channel, GPIO.OUT)
 
     def power_on(self):
         GPIO.output(self.channel, GPIO.HIGH)
