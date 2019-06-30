883668b4f27a1c6cd7b06311d03e40922a1cd3b6
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-06-30 23:23:18 +0300

    eferf

--- examples/main.py
+++ examples/main.py
@@ -1,11 +1,29 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from multiprocessing import Process
 from display import Display
+from rotary_encoder_right import RotaryEncoderRight
 
 def main():
+    runInParallel(start_encoders, start_display)
+
+def start_encoders():
+    encoder_right = RotaryEncoderRight()
+    encoder_right.start()
+
+def start_display():
     display = Display()
     display.start()
+
+def runInParallel(*fns):
+  proc = []
+  for fn in fns:
+    p = Process(target=fn)
+    p.start()
+    proc.append(p)
+  for p in proc:
+    p.join()
 
 if __name__ == "__main__":
     try:
