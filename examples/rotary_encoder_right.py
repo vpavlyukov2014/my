e8ca2ba2fd829f2f7f8311d4bc363a2e83da15c0
vpavlyukov2014 <pavlyukov@uchi.ru>
2019-11-17 18:41:13 +0300

    rtefgtgrtg

--- examples/rotary_encoder_right.py
+++ examples/rotary_encoder_right.py
@@ -20,16 +20,16 @@ class RotaryEncoderRight:
         GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def start(self):
-        GPIO.add_event_detect(self.clockPin, GPIO.FALLING, callback=self._clockCallback, bouncetime=250)
-        GPIO.add_event_detect(self.switchPin, GPIO.FALLING, callback=self._switchCallback, bouncetime=300)
+        GPIO.add_event_detect(self.clk, GPIO.FALLING, callback=self._clockCallback, bouncetime=250)
+        GPIO.add_event_detect(self.sw, GPIO.FALLING, callback=self._switchCallback, bouncetime=300)
 
     def stop(self):
-        GPIO.remove_event_detect(self.clockPin)
-        GPIO.remove_event_detect(self.switchPin)
+        GPIO.remove_event_detect(self.clk)
+        GPIO.remove_event_detect(self.sw)
         GPIO.cleanup()
 
     def _clockCallback(self):
-        if GPIO.input(self.clockPin) == 0:
+        if GPIO.input(self.clk) == 0:
             data = GPIO.input(self.dataPin)
             if data == 1:
                 self.vol_commands.vol_minus()
@@ -37,6 +37,6 @@ class RotaryEncoderRight:
                 self.vol_commands.vol_plus()
 
     def _switchCallback(self):
-        if GPIO.input(self.switchPin) == 0:
+        if GPIO.input(self.sw) == 0:
             self.switchCallback()
             self.vol_commands.toggle_play()
